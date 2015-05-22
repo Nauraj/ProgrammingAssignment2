0988c54a47f054e9e003c958574f6abedd283480
Nauraj <nauraj1@gmail.com>
2015-05-22 18:47:31 -0400

    HW assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function proceeds time-consuming computations
+## 
 
-## Write a short comment describing this function
-
+## I have taken a lot of help from the example of assignment2
+## :)
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    set_mean_matrix <- function(MeanVal) m <<- MeanVal
+    get_mean_matrix <- function() m
+    list(set = set, get = get,
+         set_mean_matrix = set_mean_matrix,
+         get_mean_matrix = get_mean_matrix)
 }
+## This part calculates the mean of given matrix.
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x = matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+    
+cachemean <- function(x, ...) {
+    m <- x$get_mean_matrix()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    solve <- x$get()
+    m <- MeanVal(solve, ...)
+    x$set_mean_matrix(m)
+    m
 }
